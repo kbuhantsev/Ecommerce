c690392b28a8e4edcbe7aa9e022923293284acf7
Konstantyn <k.buhantsev@gmail.com>
2023-08-04 12:23:22 +0300

    all products

---
+++ components/ProductsGrid.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import ProductBox from "./ProductBox";
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 20px;
+`;
+
+export default function ProductsGrid({ products }) {
+  return (
+    <Grid>
+      {products.length > 0 &&
+        products.map((product) => (
+          <ProductBox key={product._id} {...product} />
+        ))}
+    </Grid>
+  );
+}
--- pages/products.js
+++ pages/products.js
@@ -1,3 +1,32 @@
-export default function ProductsPage() {
-  return <>Products page</>;
+import Center from "@/components/Center";
+import Header from "@/components/Header";
+import ProductsGrid from "@/components/ProductsGrid";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+`;
+
+export default function ProductsPage({ products }) {
+  return (
+    <>
+      <Header />
+      <Center>
+        <Title>All products</Title>
+        <ProductsGrid products={products} />
+      </Center>
+    </>
+  );
 }
+
+export const getServerSideProps = async () => {
+  await mongooseConnect();
+  const products = await Product.find({}, null, { sort: { _id: -1 } });
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+    },
+  };
+};
